8ea92a83a20ca88ae360f600c4172f0a42f90ccc
Aid-k <aidynky99@gmail.com>
2022-08-25 21:14:32 +1000

    chore: basic styling for input boxes

--- src/components/GoalEntry.jsx
+++ src/components/GoalEntry.jsx
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../styles/styles.css";
 
 class GoalEntry extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <div id="goalEntryDiv">
-                <form action="">
-                    <label for="goal">Please enter a goal:</label>
-                    <input type="text" id="goal">
-                    </input>
-                </form>
-            </div>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <div id="goalEntryDiv" class="grid grid-cols-2 gap-4 place-items-center">
+        <div>
+          <form action="" class="text-center">
+            <label for="goal">Please enter a goal:</label>
+            <br></br>
+            <input type="text" id="goal" class="rounded-lg"></input>
+          </form>
+        </div>
+        <div>
+          <form action="" class="text-center">
+            <label for="goalDate">
+              When would you like to achieve this goal by?
+            </label>
+            <br></br>
+            <input type="text" id="goalDate" class="rounded-lg"></input>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default GoalEntry;
