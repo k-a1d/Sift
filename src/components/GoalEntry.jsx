6f9c0a021ffefee514675c3226f235a00aa3cc8a
Aidyn Ky <aidyn-ky@Aidyns-Air.modem>
2022-08-27 15:25:25 +1000

    chore: minor div change for GoalEntry

--- src/components/GoalEntry.jsx
+++ src/components/GoalEntry.jsx
@@ -5,21 +5,21 @@ class GoalEntry extends Component {
   state = {};
   render() {
     return (
-      <div id="goalEntryDiv" class="grid grid-cols-2 gap-4 place-items-center">
+      <div id="goalEntryDiv" className="h-48 grid grid-cols-2 place-items-center content-center">
         <div>
-          <form action="" class="text-center">
+          <form action="" className="text-center">
             <label for="goal">Please enter a goal:</label>
             <br></br>
-            <input type="text" id="goal" class="rounded-lg"></input>
+            <input type="text" id="goal" className="rounded-lg"></input>
           </form>
         </div>
         <div>
-          <form action="" class="text-center">
+          <form action="" className="text-center">
             <label for="goalDate">
               When would you like to achieve this goal by?
             </label>
             <br></br>
-            <input type="text" id="goalDate" class="rounded-lg"></input>
+            <input type="text" id="goalDate" className="rounded-lg"></input>
           </form>
         </div>
       </div>
